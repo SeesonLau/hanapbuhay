5b29c961c5a8a77d74b55845ead420a236c66d4f
John Laurence Sison <seesonjohnlau@gmail.com>
2025-09-13 23:13:34 +0800

    [FINAL FIX] css build error

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Add webpack config to handle Lightning CSS binary
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // Force binary resolution
-      '../lightningcss.linux-x64-gnu.node': '../lightningcss.linux-x64-musl.node'
-    };
-    return config;
-  }
 };
 
 export default nextConfig;
