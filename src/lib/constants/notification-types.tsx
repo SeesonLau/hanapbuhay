032c325ab725d2c45233d135579e9730ba81d12a
Dawson P. Alegarbes <dosonshi@gmail.com>
2025-12-12 13:19:50 +0800

    add/notif

--- src/lib/constants/notification-types.tsx
+++ src/lib/constants/notification-types.tsx
@@ -1,11 +1,15 @@
 export enum NotificationType {
   JOB_APPLICATION = "JOB_APPLICATION",
   APPLICATION_ACCEPTED = "APPLICATION_ACCEPTED",
+  APPLICATION_REJECTED = "APPLICATION_REJECTED",
+  APPLICATION_COMPLETED = "APPLICATION_COMPLETED",
   NEW_MESSAGE = "NEW_MESSAGE"
 }
 
 export const NOTIFICATION_ROUTES: Record<NotificationType, string> = {
   [NotificationType.JOB_APPLICATION]: "/manageJobPosts",
   [NotificationType.APPLICATION_ACCEPTED]: "/appliedJobs",
+  [NotificationType.APPLICATION_REJECTED]: "/appliedJobs",
+  [NotificationType.APPLICATION_COMPLETED]: "/appliedJobs",
   [NotificationType.NEW_MESSAGE]: "/chat"
 };  
